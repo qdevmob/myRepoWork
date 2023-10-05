816c131e72093ab7306d0efafe4d4413e44ece7c
= <=>
2023-10-05 15:38:11 +0300

    commit repo

---
+++ app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.example.myapplication
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+//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
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
